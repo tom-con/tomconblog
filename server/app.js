595f79b188a5b126e749e5ad34adff06c3dd9650
Tom Conchie <tconchie@gmail.com>
2017-05-13 18:49:12 -0600

    Initializes express and angular, sets up file structure

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+
+const index = require('./routes/index')
+const posts = require('./routes/posts');
+
+
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/../client')))
+
+app.use('/index', index)
+app.use('/api/posts', posts)
+
+app.use('*', function (req, res) {                            // <--- #2
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '/../client')
+  })
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
